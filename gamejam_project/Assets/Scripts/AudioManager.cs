f7f83ceda8b96e44175f571301481cfe9ad62822
hgghhggh <116894625+meumeu21@users.noreply.github.com>
2024-04-13 18:25:42 +0300

    settings-music-added

---
+++ gamejam_project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    public AudioClip music;
+    public AudioClip rainSFX;
+    public AudioClip peopleSFX;
+
+    private void Start()
+    {
+        musicSource.clip = music;
+        musicSource.Play();
+    }
+
+    public void PlayRain()
+    {
+        sfxSource.clip = rainSFX;
+        sfxSource.Play();
+    }
+
+    public void PlayPeople()
+    {
+        sfxSource.clip = peopleSFX;
+        sfxSource.Play();
+    }
+}
